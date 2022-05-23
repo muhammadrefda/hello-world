50082ec75780450e32d3bab4509123fa6efe442d
Administrator <administrator@tradecorp.hk>
2022-05-23 17:00:56 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+func main() {
+	//var firstName string = "John"
+	//var middleName = "Paul"
+	//lastName := "Doe"
+	//lastName = "Smith"
+
+	//fmt.Printf("Hello %s %s %s!\n", firstName, middleName, lastName)
+
+	/*
+		var first, second, third string
+		first, second, third = "John", "Paul", "Doe"
+		fmt.Println(first, second, third)
+	*/
+
+	//var fourth, fifth, sixth string = "mick", "stanley", "harrison"
+	//fmt.Println(fourth, fifth, sixth)
+
+	//seventh, eighth, ninth := "jagger", "jhonny", "james"
+	//fmt.Println(seventh, eighth, ninth)
+	//
+	//number, day, year := 1, "Monday", 2019
+	//fmt.Println(number, day, year)
+
+	//_ = "doni"
+	//_ = "dodi"
+	//name, _ := "anwar", "sudarsono"
+	//fmt.Println(name, _)
+
+	//name := new(string)
+	//fmt.Println(name)
+	//fmt.Println(*name)
+
+	/*
+		var positiveNumber uint8 = 89
+		var negativeNumber int8 = -89
+		fmt.Println("bilangan positif:", positiveNumber)
+		fmt.Println("bilangan negatif:", negativeNumber)
+	*/
+
+	//var decimalNumber = 2.62
+	//fmt.Printf("bilangan decimal: %f\n", decimalNumber)
+	//fmt.Printf("bilangan decimal: %.3f\n", decimalNumber)
+	//fmt.Println("bilangan decimal:", decimalNumber)
+
+	//	var exist bool = true
+	//	fmt.Printf("exist? %t \n", exist)
+	//
+	//	var message = `My Name is
+	//jhonny`
+	//
+	//	fmt.Println(message)
+
+	//fmt.Println("jhonny deep")
+	//fmt.Println("john", "wick")
+
+	//fmt.Print("deddy\n")
+	//fmt.Print("dodo ", "wick")
+	//fmt.Print("john", " ", "wick\n")
+
+	//var value = (((2+6)%3)*4 - 2) / 3
+	//var isEqual = (value == 2)
+
+	//fmt.Printf("nilai %d (%t) \n", value, isEqual)
+
+	//var left = false
+	//var right = true
+
+	//var leftAndRight = left && right
+	//fmt.Printf("left && right \t(%t) \n", leftAndRight)
+	//
+	//var leftOrRight = left || right
+	//fmt.Printf("left || right \t(%t) \n", leftOrRight)
+	//
+	//var leftReverse = !left
+	//fmt.Printf("!left \t\t(%t) \n", leftReverse)
+
+	//var point = 3
+	//
+	//if point == 10 {
+	//	fmt.Println("lulus dgn nilai sempurna")
+	//} else if point > 5 {
+	//	fmt.Println("lulus")
+	//} else if point == 4 {
+	//	fmt.Println("hampir lulus")
+	//} else {
+	//	fmt.Println("tidak lulus")
+	//}
+
+	//var point = 8840.0
+	//if percent := point / 100; percent >= 100 {
+	//	fmt.Printf("%.1f%s perfect!\n", percent, "%")
+	//} else if percent >= 70 {
+	//	fmt.Printf("%.1f%s good\n", percent, "%")
+	//} else {
+	//	fmt.Printf("%.1f%s not bad\n", percent, "%")
+	//}
+
+	//var point = 6
+
+	//switch point {
+	//case 8:
+	//	fmt.Println("lulus dgn nilai sempurna")
+	//case 7:
+	//	fmt.Println("lulus")
+	//default:
+	//	fmt.Println("tidak lulus")
+	//}
+
+	//switch {
+	//case point == 3:
+	//	fmt.Println("lulus dgn nilai sempurna")
+	//case (point > 5) && (point < 8):
+	//	fmt.Println("lulus")
+	//default:
+	//	{
+	//		fmt.Println("tidak lulus")
+	//		fmt.Println("you failed")
+	//	}
+	//}
+
+	//switch {
+	//case point == 8:
+	//	fmt.Println("lulus dgn nilai sempurna")
+	//case (point < 8) && (point > 3):
+	//	fmt.Println("lulus")
+	//	fallthrough
+	//case point < 5:
+	//	fmt.Println("hampir lulus")
+	//default:
+	//	{
+	//		fmt.Println("tidak lulus")
+	//		fmt.Println("you failed")
+	//	}
+	//}
+
+	//var point = 1
+
+	//if point > 7 {
+	//	switch point {
+	//	case 10:
+	//		fmt.Println("perfect")
+	//	default:
+	//		fmt.Println("good")
+	//	}
+	//} else {
+	//	if point == 5 {
+	//		fmt.Println("not bad")
+	//	} else if point == 3 {
+	//		fmt.Println("keep trying")
+	//	} else {
+	//		fmt.Println("you failed")
+	//		if point == 0 {
+	//			fmt.Println("you got zero")
+	//		}
+	//	}
+	//}
+
+	//for i := 0; i <= 5; i++ {
+	//	fmt.Println(i)
+	//}
+
+	//var i = 0
+
+	//for i < 5 {
+	//	fmt.Println("Angka", i)
+	//	i++
+	//	if i == 5 {
+	//	}
+	//}
+
+	//var i = 0
+
+	//for {
+	//	fmt.Println("Angka", i)
+	//
+	//	i++
+	//
+	//	if i == 55 {
+	//		break
+	//	}
+	//}
+
+	//for i := 0; i < 10; i++ {
+	//	if i%2 == 0 {
+	//		continue
+	//	}
+	//
+	//	if i > 8 {
+	//		break
+	//	}
+	//
+	//	fmt.Println("Angka", i)
+	//}
+
+}
